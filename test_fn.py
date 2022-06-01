3f7ac0849c87c2193ca798783a085075e8e6758a
AnderP-cod <nemo001leyn@gmail.com>
2022-06-01 16:38:50 +0200

    Create test_fn.py

---
+++ test_fn.py
@@ -0,0 +1,15 @@
+import unittest
+import fn
+
+
+class Testfn(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(fn.add(10, 20), 30)
+
+    def test_subtract(self):
+        self.assertEqual(fn.subtract(2, 2), 0)
+
+    def test_multiply(self):
+        self.assertEqual(fn.multiply(2, 2), 4)
+
+
