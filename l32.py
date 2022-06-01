05939448eb1198bea2ac7fbdaa44519fe4dc3969
AnderP-cod <nemo001leyn@gmail.com>
2022-06-01 16:38:43 +0200

    Create l32.py

---
+++ l32.py
@@ -0,0 +1,12 @@
+
+
+def numbers1():
+    """Ввод 1 числа"""
+    number1 = int(input("Введите число 1: "))
+    return number1
+
+
+def numbers2():
+    """Ввод 2 числа"""
+    number2 = int(input("Введите число 2: "))
+    return number2
